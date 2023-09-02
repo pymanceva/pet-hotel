d180cbf27f464b89afd9bb4f24049fba52823e2c
Olesia Rumiantseva <pymanceva@yandex.ru>
2023-09-02 14:21:11 +0300

    Рабочая ветка develop

---
+++ src/main/java/ru/dogudacha/PetHotel/PetHotelApplication.java
@@ -0,0 +1,13 @@
+package ru.dogudacha.PetHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHotelApplication.class, args);
+	}
+
+}
