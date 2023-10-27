3f45fdaf5fe40830582ae90ee703f84ecece04f7
alisilakg <wisper14@yandex.ru>
2023-10-27 15:06:31 +0500

    add comments

--- src/main/java/ru/dogudacha/PetHotel/pet/mapper/PetMapper.java
+++ src/main/java/ru/dogudacha/PetHotel/pet/mapper/PetMapper.java
@@ -1,6 +1,5 @@
 package ru.dogudacha.PetHotel.pet.mapper;
 
-import lombok.experimental.UtilityClass;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.dogudacha.PetHotel.pet.dto.NewPetDto;
@@ -12,66 +11,7 @@ import ru.dogudacha.PetHotel.pet.model.Pet;
 public interface PetMapper {
     PetDto toPetDto(Pet pet);
     PetForAdminDto toPetForAdminDto(Pet pet);
-
-//    RoomWithoutPriceDto toRoomDtoWithoutPrice(Room room);
-
     @Mapping(target = "id", ignore = true)
     Pet toPet(NewPetDto newPetDto);
-
-//    @Mapping(target = "id", ignore = true)
-//    Pet toRoom(UpdateRoomDto roomDto);
-//
-//    List<RoomDto> toListRoomDto(List<Room> rooms);
-//
-//    List<RoomWithoutPriceDto> toListRoomWithoutPriceDto(List<Room> rooms);
-
-//    public Pet toPet(NewPetDto newPetDto) {
-//        return Pet.builder()
-//                .typeOfPet(newPetDto.getTypeOfPet())
-//                .breed(newPetDto.getBreed())
-//                .sex(newPetDto.getSex())
-//                .age(newPetDto.getAge())
-//                .weight(newPetDto.getWeight())
-//                .diet(newPetDto.getDiet())
-//                .isTakesMedications(newPetDto.getIsTakesMedications())
-//                .isContact(newPetDto.getIsContact())
-//                .isPhotographed(newPetDto.getIsPhotographed())
-//                .comments(newPetDto.getComments())
-//                .build();
-//    }
-//
-//    public PetDto toPetDto(Pet pet) {
-//        return PetDto.builder()
-//                .id(pet.getId())
-//                .typeOfPet(pet.getTypeOfPet())
-//                .breed(pet.getBreed())
-//                .sex(pet.getSex())
-//                .age(pet.getAge())
-//                .weight(pet.getWeight())
-//                .diet(pet.getDiet())
-//                .isTakesMedications(pet.getIsTakesMedications())
-//                .isContact(pet.getIsContact())
-//                .isPhotographed(pet.getIsPhotographed())
-//                .comments(pet.getComments())
-//                .build();
-//    }
-//
-//    public PetForAdminDto toPetForAdminDto(Pet pet) {
-//        return PetForAdminDto.builder()
-//                .id(pet.getId())
-//                .typeOfPet(pet.getTypeOfPet())
-//                .breed(pet.getBreed())
-//                .sex(pet.getSex())
-//                .age(pet.getAge())
-//                .weight(pet.getWeight())
-//                .diet(pet.getDiet())
-//                .isTakesMedications(pet.getIsTakesMedications())
-//                .isContact(pet.getIsContact())
-//                .isPhotographed(pet.getIsPhotographed())
-//                .comments(pet.getComments())
-//                .historyOfBookings(null)
-//                .additionalServices(null)
-//                .build();
-//    }
 }
 
